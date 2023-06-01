092102b8d9b8a9a13c3dd1afb0ffe70b0aa0fc6a
Dusan <dusan.daniska@wai.sk>
2023-06-01 16:48:03 +0200

    build-prototype.bat fix

--- bin/build-prototype.bat
+++ bin/build-prototype.bat
@@ -2,9 +2,9 @@
 
 set /p "rewriteBase=Enter the rewrite base for your project. Must start and end with '/'. E.g /adios-empty-app/: "
 
-php ..\vendor\wai-blue\adios\src\CLI\build-prototype.php ^
+php %~dp0../vendor/wai-blue/adios/src/CLI/build-prototype.php ^
   --salt adios-empty-app ^
-  --input-file ..\vendor\wai-blue\adios\docs\Prototype\examples\10-simple-crm.json ^
+  --input %~dp0../vendor/wai-blue/adios/docs/Prototype/examples/10-simple-crm.json ^
   --rewrite-base %rewriteBase% ^
   --root-url http://localhost%rewriteBase% ^
-  --output-folder ..
+  --output-folder %~dp0..
