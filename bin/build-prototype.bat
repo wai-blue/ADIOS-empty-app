a6928589f6c169a1186b4b11b92a07594652255f
WAI Blue <88132073+wai-blue@users.noreply.github.com>
2023-06-01 20:42:10 +0200

    Update build-prototype.bat

--- bin/build-prototype.bat
+++ bin/build-prototype.bat
@@ -3,7 +3,7 @@
 set /p "rootUrl=Enter the root URL for your project. Must NOT start and end with '/'. E.g my-projects/adios-empty-app: "
 
 php %~dp0../vendor/wai-blue/adios/src/CLI/build-prototype.php ^
-  --salt adios-empty-app ^
+  --salt adios-app ^
   --input %~dp0../vendor/wai-blue/adios/docs/Prototype/examples/10-simple-crm.json ^
   --rewrite-base /%rootUrl%/ ^
   --root-url http://localhost/%rootUrl% ^
