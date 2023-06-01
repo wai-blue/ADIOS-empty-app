f7323b3336398f30908e93b20543a5a6512ff217
Dusan <dusan.daniska@wai.sk>
2023-06-01 21:17:37 +0200

    build command optimization

--- bin/build.bat
+++ bin/build.bat
@@ -1,10 +1,5 @@
 @echo off
 
-set /p "rootUrl=Enter the root URL for your project. Must NOT start and end with '/'. E.g my-projects/adios-empty-app: "
-
 php %~dp0../vendor/wai-blue/adios/src/CLI/build-prototype.php ^
-  --salt adios-app ^
   --input %~dp0../vendor/wai-blue/adios/docs/Prototype/examples/10-simple-crm.json ^
-  --rewrite-base /%rootUrl%/ ^
-  --root-url http://localhost/%rootUrl% ^
   --output-folder %~dp0..
