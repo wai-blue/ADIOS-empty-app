fac5cea25ffa98fafbba7a95a59e421de6c27032
Dusan <dusan.daniska@wai.sk>
2023-06-09 14:06:44 +0200

    yaml added to composer.json

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-php ./vendor/wai-blue/adios/src/CLI/build-prototype.php \
+php ./../vendor/wai-blue/adios/src/CLI/build-prototype.php \
   --input ./../vendor/wai-blue/adios/docs/Prototype/examples/10-simple-crm.json \
   --output-folder ..
