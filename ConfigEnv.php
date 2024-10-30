85d3f6d0a5816f61f8c26717a0778598f1421111
Michal Blava <michal.blava@gmail.com>
2024-10-30 14:07:46 +0100

    file structure

---
+++ ConfigEnv.php
@@ -0,0 +1,21 @@
+<?php
+
+// db
+$config["db_host"]             = "localhost";
+$config["db_user"]             = "root";
+$config["db_password"]         = "";
+$config["db_name"]             = "adios-app";
+$config["db_codepage"]         = "utf8mb4";
+$config["global_table_prefix"] = "";
+
+$config['db']['provider']      = "MySQLi";
+$config['db']['dsn']           = "";
+
+// dirs & urls
+$config['rewriteBase']         = "/adios-app/";
+$config["url"]                 = "http://localhost/adios-app/";
+
+// misc
+$config['develMode']           = TRUE;
+$config['language']            = "sk";
+
