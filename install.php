3761f773c4b48f03543e1d80edb9dee5d14d0650
Michal Blava <michal.blava@gmail.com>
2024-11-08 18:47:28 +0100

    ceremony install script

--- install.php
+++ install.php
@@ -1,24 +1,28 @@
 <?php
 
-ini_set('html_errors', "1");
-ini_set('error_prepend_string', "<pre style='color: #333; font-face:monospace; font-size:8pt;'>");
-ini_set('error_append_string', "</pre>");
-
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL ^ E_NOTICE ^ E_WARNING);
-
-define('_ADIOS_ID', 'securityagency');
-
 // load configs
 require_once(__DIR__ . "/ConfigEnv.php");
 require_once(__DIR__ . "/src/ConfigApp.php");
 
-// echo password_hash('administrator', PASSWORD_DEFAULT);exit;
-
 // load application class
-require("src/App.php");
+require(__DIR__ . "/src/App.php");
 
-// render
-$app = new AdiosApp($config);
-// list($route, $params) = $app->extractRouteParamsFromRequest(); _var_dump($app->router->applyRouting($route, $params));exit;
-$app->install();
+try {
+  $app = new AdiosApp($config, TRUE);
+  $app->install();
+
+  $mUser = new \ADIOS\Models\User($app);
+  $idUserAdministrator = $mUser->eloquent->create([
+    'login' => 'administrator',
+    'password' => $mUser->hashPassword('administrator'),
+    'is_active' => 1,
+  ])->id;
+
+//  $mUserRole = new \CeremonyCrmApp\Modules\Core\Settings\Models\UserRole($app);
+//  $idRoleAdministrator = $mUserRole->eloquent->create(['name' => 'Administrator'])->id;
+
+//  $mUserHasRole = new \CeremonyCrmApp\Modules\Core\Settings\Models\UserHasRole($app);
+//  $mUserHasRole->eloquent->create(['id_user' => $idUserAdministrator, 'id_role' => $idRoleAdministrator])->id;
+} catch (\Exception $e) {
+  echo $e->getMessage();
+}
